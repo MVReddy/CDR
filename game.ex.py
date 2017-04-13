0caacb82d9f425dde871314745fa09084986d5e3
Venkata Reddy M <venkatareddy.mulam@techmahindra.com>
2017-04-13 11:37:15 -0400

    Create game.ex.py

---
+++ game.ex.py
@@ -0,0 +1,11 @@
+# tkexample.py
+"""A very basic Tkinter example. """
+
+import Tkinter
+from Tkinter import *
+
+
+root = Tk()
+root.title('A Tk Application')
+Label(text='I am a label').pack(pady=15)
+root.mainloop()
